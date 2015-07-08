0a1680579f063d5b0469d872232daf7d7636fd99
Alexandra Popescu <lixapopescu@gmail.com>
2015-07-08 18:14:29 +0200

    bugfix: redirect when no plan for interval

--- modules/planning/server/controllers/recipes/recipes.weekly.server.controller.js
+++ modules/planning/server/controllers/recipes/recipes.weekly.server.controller.js
@@ -16,6 +16,7 @@ var //_ = require('lodash'),
 exports.find = function(req, res) {
     var username = req.user.username;
     // console.log('recipes.weekly.server.controller ', username);
+    console.log(Utils.getDateFromString(req.params.start_year, req.params.start_month, req.params.start_day), Utils.getDateFromString(req.params.end_year, req.params.end_month, req.params.end_day));
     var planning = Planning.find({
             date: {
                 $gte: Utils.getDateFromString(req.params.start_year, req.params.start_month, req.params.start_day),
@@ -33,6 +34,7 @@ exports.find = function(req, res) {
         },
         function(err, data) {
             if (!err) {
+                console.log(data);
                 res.json(data);
             } else {
                 res.status(400).send({
