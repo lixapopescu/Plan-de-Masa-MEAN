a19605134fa1c7bb7c521d04dd726ae15952920e
Alexandra Popescu <lixapopescu@gmail.com>
2015-07-09 15:41:34 +0200

    header cleanup

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,23 +1,45 @@
 'use strict';
 
+function gotoRecipePlanning($state, interval) {
+    $state.go('top.plan.detail', {
+        start_year: interval.startDate.year(),
+        start_month: interval.startDate.month() + 1,
+        start_day: interval.startDate.date(),
+        end_year: interval.endDate.year(),
+        end_month: interval.endDate.month() + 1,
+        end_day: interval.endDate.date()
+    });
+}
+
+function gotoSettings($state) {
+    $state.go('top.settings.profile');
+}
+
 angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
-	function($scope, $state, Authentication, Menus) {
-		// Expose view variables
-		$scope.$state = $state;
-		$scope.authentication = Authentication;
+    function($scope, $state, Authentication, Menus) {
+        // Expose view variables
+        $scope.$state = $state;
+        $scope.authentication = Authentication;
+        $scope.gotoRecipePlanning = gotoRecipePlanning;
+        $scope.gotoSettings = gotoSettings;
 
-		// Get the topbar menu
-		$scope.menu = Menus.getMenu('topbar');
+        // Get the topbar menu
+        $scope.menu = Menus.getMenu('topbar');
 
-		// Toggle the menu items
-		$scope.isCollapsed = false;
-		$scope.toggleCollapsibleMenu = function() {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};
+        // Toggle the menu items
+        $scope.isCollapsed = false;
+        $scope.toggleCollapsibleMenu = function() {
+            $scope.isCollapsed = !$scope.isCollapsed;
+        };
 
-		// Collapsing the menu after navigation
-		$scope.$on('$stateChangeSuccess', function() {
-			$scope.isCollapsed = false;
-		});
-	}
+        // Collapsing the menu after navigation
+        $scope.$on('$stateChangeSuccess', function() {
+            $scope.isCollapsed = false;
+        });
+
+        $scope.interval = {
+            startDate: moment(),
+            endDate: moment().add(6, 'days')
+        };
+    }
 ]);
