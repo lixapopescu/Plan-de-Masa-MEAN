de9af6bfa849c5dc408b560f4264f1430fe39453
Alexandra Popescu <lixapopescu@gmail.com>
2015-09-19 20:14:52 +0200

    Autocomplete dialog working. Generic, on top of all recipes.

--- modules/planning/client/controllers/RecipesController.js
+++ modules/planning/client/controllers/RecipesController.js
@@ -1,133 +1,106 @@
 'use strict';
 
-function changeInterval(intervalFromScope, interval) {
-    console.log(interval);
-    intervalFromScope = interval;
-    // $state.go('top.plan.detail', {
-    //     start_year: interval.startDate.year(),
-    //     start_month: interval.startDate.month() + 1,
-    //     start_day: interval.startDate.date(),
-    //     end_year: interval.endDate.year(),
-    //     end_month: interval.endDate.month() + 1,
-    //     end_day: interval.endDate.date()
-    // });
-
-}
-
-function getAutocompleteItems(searchText) {
-    return ['pui', 'paste', 'acru', 'picnic', 'de vara'];
-}
-
 /**
  * Create filter function for a query string. Match anywhere in the string (>=0 condition)
  */
 function createFilterFor(query) {
-    var lowercaseQuery = angular.lowercase(query);
-    // $log.info(query);
-    return function filterFn(state) {
-        return (state.value.indexOf(lowercaseQuery) >= 0);
-    };
+  var lowercaseQuery = angular.lowercase(query);
+  return function filterFn(state) {
+    return (state.value.indexOf(lowercaseQuery) >= 0);
+  };
 }
 
-
-
 angular.module('planning').controller('RecipesController', ['$scope', '$rootScope', '$stateParams', '$q', '$timeout', '$state', '$log', 'Recipes', 'Labels',
-    function($scope, $rootScope, $stateParams, $q, $timeout, $state, $log, Recipes, Labels) {
-        console.log('in RecipesController', $stateParams, $scope.$id);
-        Recipes.query({
-                start_year: $stateParams.start_year,
-                start_month: $stateParams.start_month,
-                start_day: $stateParams.start_day,
-                end_year: $stateParams.end_year,
-                end_month: $stateParams.end_month,
-                end_day: $stateParams.end_day
-            },
-            function(data) {
-                //if the Array returned is actually an array of objects or just a empty promise 
-                // console.log(data);
-                if (data[0]) {
-                    // console.log('RecipesController', data);
-                    $scope.dailyPlans = data;
-                    $scope.date = getInterval($scope.dailyPlans); //for calendar input control
-                    moment.locale('ro');
-                    $scope.opts = CALENDAR_OPTIONS;
-                } else {
-                    $scope.noPlan = true;
-                }
-            },
-            function(err) {
-                //handle error
-            });
+  function ($scope, $rootScope, $stateParams, $q, $timeout, $state, $log, Recipes, Labels) {
+    console.log('in RecipesController', $stateParams, $scope.$id);
 
-        //*********************************************************
-        Labels.query({},
-            function(data) {
-                // console.log('Labels', data);
-                $scope.labels = data;
-            },
-            function(err) {
-                //handle err
-            });
-
-        $scope.thisLabel = $scope.labels;
-
-
-        var self = this;
-        $scope.selectedLabels = [];
-        // ******************************
-        // Internal methods
-        // ******************************
-
-        function searchTextChange(text) {
-            $log.info('Text changed to ' + text);
+    /**
+     * Call the Recipe service to retrieve the recipes coresponding to the interval selected in $stateParams
+     * @return {Array<Json>}              fills in dailyPlans from $scope
+     */
+    Recipes.query({
+        start_year: $stateParams.start_year,
+        start_month: $stateParams.start_month,
+        start_day: $stateParams.start_day,
+        end_year: $stateParams.end_year,
+        end_month: $stateParams.end_month,
+        end_day: $stateParams.end_day
+      },
+      function (data) {
+        //if the Array returned is actually an array of objects or just a empty promise
+        if (data[0]) {
+          $scope.dailyPlans = data;
+          $scope.date = getInterval($scope.dailyPlans); //for calendar input control
+          moment.locale('ro');
+          $scope.opts = CALENDAR_OPTIONS;
+        } else {
+          $scope.noPlan = true;
         }
+      },
+      function (err) {
+        //handle error
+      });
 
-        function selectedItemChange(item) {
-            $log.info('Item changed to ' + JSON.stringify(item));
-            // $scope.selectedLabels.push(item);
-            $scope.thisLabel.pull(item);
-        }
+  /**
+   * Retrieve labels RESTful API through Angular service 'Label'
+   * @param  {Query Parameters} {}       Query all labels available
+   * @return {autocomplete.labels}          Fill in labels for autocomplete
+   */
+    Labels.query({},
+      function (data) {
+        $scope.autocomplete.labels = data;
+      },
+      function (err) {
+        //handle err
+      });
 
-        self.selectedItemChange = selectedItemChange;
-        self.querySearch = querySearch;
-        // self.searchTextChange = searchTextChange;
+    /**
+     * Watch interval change on date-picker. Necessary because the date-picker snippet is in another view
+     * @return {Moment}                   Updates the date object from $scope
+     */
+    $rootScope.$on('interval.change', function (event, interval) {
+      $scope.date = interval;
+    });
 
-        /**
-         * Return results for autocomplete list
-         */
-        function querySearch(query) {
-            var results = query ? $scope.labels.filter(createFilterFor(query)) : $scope.labels;
-            return results;
-        }
+    $scope.Math = window.Math;
+    $scope.getDistanceLabel = getDistanceLabel;
+    // $scope.changeInterval = changeInterval;
 
+    /**
+     * Autocomplete object for use in md-autocomplete. Groups all general settings
+     * @type {Json}
+     */
+    $scope.autocomplete = {
+      readonly: false,
+      selectedItem: null,
+      searchText: null,
+      querySearch: querySearch,
+      selectedItemChange: selectedItemChange,
+      selectedLabels: []
+    };
 
-
-
-        //*********************************************************
-
-
-
-
-        $scope.Math = window.Math;
-        $scope.getDistanceLabel = getDistanceLabel;
-        $scope.changeInterval = changeInterval;
-        $scope.autocomplete = {
-            enabled: true,
-            noCache: false,
-            searchText: '',
-            selectedItem: '',
-            // selectedItemChange: selectedItemChange,
-            // searchTextChange: searchTextChange,
-            // querySearch: querySearch
-        };
-
-        $rootScope.$on('interval.change', function(event, interval) {
-            $scope.date = interval;
-        });
-
-        // $scope.$watch('date', function(interval) {
-        //     console.log('New date set: ', interval);
-        //     // generatePlan($scope.date, $scope.globalPlan, $scope.dailyPlans, $scope.RandomRecipeService);
-        // }, false);
+    /**
+     * Called each time an item is added in autocomplete
+     * @param  {[type]} item [description]
+     * @return {[type]}      [description]
+     */
+    function selectedItemChange(item) {
+      $log.info('selectedLabels ' + $scope.autocomplete.selectedLabels.length);
+      // var obj = $scope.autocomplete.selectedLabels[0];
+      // if (obj) $log.info('name: ' + obj['name']);
+      // if (item) {
+      //   $log.info('Item changed to ' + JSON.stringify(item));
+      //   if (item.$$hashKey)
+      //     $log.info('Used before ' + item.name);
+      // }
     }
+
+    /**
+     * Generate autocomplete query.
+     */
+    function querySearch(query) {
+      return query ? $scope.autocomplete.labels.filter(createFilterFor(query)) : [];
+    }
+
+  }
 ]);
